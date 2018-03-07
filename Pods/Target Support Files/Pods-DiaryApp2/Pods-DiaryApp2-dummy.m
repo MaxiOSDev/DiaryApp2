66dc9a066d57790d0270a5fc8d6db45725330ab7
Max Ramirez <ramirezmax1998@gmail.com>
2018-03-07 11:14:25 -0500

    Initial Commit

---
+++ Pods/Target Support Files/Pods-DiaryApp2/Pods-DiaryApp2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaryApp2 : NSObject
+@end
+@implementation PodsDummy_Pods_DiaryApp2
+@end
---
+++ Pods/Target Support Files/Pods-DiaryApp2Tests/Pods-DiaryApp2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaryApp2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DiaryApp2Tests
+@end
